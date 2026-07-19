39d828104fd8a48f09887819c2ccf8231535bb8d
CAEK Codex <caekengineering-ui@users.noreply.github.com>
2026-07-19 18:13:16 +0100

    Simplifier l ecran gestion dechets

--- service-worker.js
+++ service-worker.js
@@ -5,7 +5,7 @@
    Pour publier une mise a jour : incrementer CACHE_VERSION.
    ============================================================ */
 
-var CACHE_VERSION = "caek-beton-v83";
+var CACHE_VERSION = "caek-beton-v84";
 
 var APP_SHELL = [
   "./",
